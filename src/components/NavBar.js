037b6732a222675986bfb61e69f6538b83fa85b1
Lara Giarrocco <laragiarrocco@gmail.com>
2022-05-04 11:05:56 -0300

    Segundo desafio

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    <header>
+        <h1>DARS</h1>
+    
+  <nav>
+      <a href="#">New in</a>
+      <a href="#">Productos</a>
+      <a href="#">Contacto</a>
+  </nav>
+  </header>
+}
+
+export default NavBar;
